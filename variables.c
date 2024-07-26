a05a1a0c51afc88ac39c608f71e942431758a6e1
shubhamjangid782 <skshubham782@gmail.com>
2024-07-26 21:48:54 +0530

    init

---
+++ variables.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a;
+    a = 6;
+    printf("the output of a is %d",a);
+    return 0;
+}
---
+++ variablesrules.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int shubham = 2;
+    int jangid = 3;
+    int third =4;
+    printf("the output of a is %d" , shubham);
+    return 0;
+}
